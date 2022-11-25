a9377e3933c8bacd504c237a4513162c3165b54d
albetnov <ryxinfrvr@gmail.com>
2022-11-25 15:22:30 +0700

    enhance link with params

--- resources/js/Components/Link.jsx
+++ resources/js/Components/Link.jsx
@@ -1,6 +1,6 @@
 import { Link as UILink } from "@chakra-ui/react";
 import { Link as RouterLink } from "@inertiajs/inertia-react";
 
-export default function Link({ to, ...props }) {
-    return <UILink {...props} href={route(to)} as={RouterLink} />;
+export default function Link({ to, params = null, ...props }) {
+    return <UILink {...props} href={route(to, params)} as={RouterLink} />;
 }
